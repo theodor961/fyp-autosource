4762b89c785593611ab70561a72ef8093d4984bc
marieangenicolas <marieangenicolas13@gmail.com>
2022-06-06 14:02:55 +0300

    commit no 9

--- src/pages/requests/RequestItem.js
+++ src/pages/requests/RequestItem.js
@@ -2,14 +2,25 @@ import React from 'react';
 import Card from "../../components/Card";
 
 import styles from './RequestItem.module.css';
+import imgAlt from '../../assets/imgAlt.png';
 
 export default function RequestItem(props) {
   return (
     <Card>
-      <div onClick={props.onClick}>
-        <img src={props.requestData.image} alt="no-image" className={styles.image} />
-        <p>{props.requestData.part_info.category} for {props.requestData.part_info.brand} {props.requestData.part_info.model}</p>
-        <p>{props.requestData.description}</p>
+      <div onClick={props.onClick} className={styles.container}>
+        <img src={props.requestData.image || imgAlt} alt="no-image" className={styles.image} />
+        <p className={styles.label}>Part name:</p>
+        <p className={styles.data}>{props.requestData.title}</p>
+        <br/> <br/>
+        <div className={styles.child1}>
+          <div className={styles.miniChild}>
+          <p className={styles.label}>Description:</p>
+          <p className={styles.data}>{props.requestData.description.substring(0,190)}...</p>
+          </div>
+        </div>
+        <div className={styles.child2}>
+          <p className={styles.date}>{props.requestData.created_at.toDate().toString().substring(0,15)}</p>
+        </div>
       </div>
     </Card>
 
